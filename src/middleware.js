45df4266b3b5938965d6db786cb836d218dba213
yama tomo <ty.summit@gmail.com>
2025-03-24 15:14:39 +0900

    0324-1

--- src/middleware.js
+++ src/middleware.js
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server"
 import { jwtVerify, SignJWT } from "jose";
 
 export async function middleware(request) {
+    console.log("middle")
     const token = await request.headers.get("Authorization")?.split(" ")[1]
     // const token = "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inpha2kzQGdtYWlsLmNvbSIsImV4cCI6MTc0MTg0Mjc4OX0.C1q6bnvsVBto0Pds3wCD2wewHiVibP-UgwUePyEiQRw"
     if(!token){
